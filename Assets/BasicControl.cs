e2602ac180746e37da84f9f02c043549869038b3
Pomelowy <mosno6@gmail.com>
2020-05-07 05:37:14 +0700

    basic movement(run,jump-hold,double jump)

--- Assets/BasicControl.cs
+++ Assets/BasicControl.cs
@@ -14,15 +14,24 @@ public class BasicControl : MonoBehaviour
     public float checkRadius;
     public LayerMask whatIsGround;
     
-    [SerializeField] int jumpCounter;
-    [SerializeField] int jumpTimes;
-    private bool isJumping;
+    private int jumpCounter;
+    public int canJumpTimes;
+    //double jump
+
+    private float jumpDuration1;
+    private float jumpDuration2;
+    public float canJumpDuration;
+    //jump higher
+    
+    
+    public bool isJumping=false;
 
     void Start()
     {
-
+        jumpDuration1 = canJumpDuration;
+        jumpDuration2 = canJumpDuration;
+        jumpCounter = canJumpTimes;
         rb = GetComponent<Rigidbody2D>();
-
     }
 
     void FixedUpdate()
@@ -30,8 +39,12 @@ public class BasicControl : MonoBehaviour
         if(isGrounded = Physics2D.OverlapCircle(groundPos.position, checkRadius, whatIsGround))
             {
             isGrounded = true;
+            
             }
-        
+        if(!isGrounded)
+        {
+            isJumping=true;
+        }
     }
 
 
@@ -40,24 +53,65 @@ public class BasicControl : MonoBehaviour
         Run();
         Jump();
 
-
+        
+        Debug.Log(moveInput);
 
     }
 
     void Jump()
     {
-        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = Vector2.up * jumpForce;
 
-            isJumping = true;
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded == true)
+        {               
+                rb.velocity = Vector2.up * jumpForce;            
+        }
+        if (Input.GetKey(KeyCode.Space)&&isJumping==true)
+        {
+            
+            if (jumpDuration1 > 0&&moveInput==0)
+            {
+                
+                rb.velocity = (Vector2.up * jumpForce);
+                jumpDuration1 -= Time.deltaTime;
+            }
+            else if (jumpDuration1 > 0 && moveInput == 1)
+            {
+
+                rb.velocity = (Vector2.up * jumpForce)+ (Vector2.right * speed);
+                jumpDuration1 -= Time.deltaTime;
+            }
+            else if (jumpDuration1 > 0 && moveInput == -1)
+            {
+
+                rb.velocity = (Vector2.up * jumpForce) + (Vector2.left * speed);
+                jumpDuration1 -= Time.deltaTime;
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)&& jumpCounter > 0 && !isGrounded)
+        {
+            
+            
+                jumpDuration2 -= Time.deltaTime;
+                rb.velocity = Vector2.up * jumpForce;
+                jumpCounter--;
+
+        }
+        if (isGrounded==true)
+        {
+            jumpCounter = canJumpTimes;
+            jumpDuration1 = canJumpDuration;
+            jumpDuration2 = canJumpDuration;
+            isJumping = false;
         }
 
     }
 
     void Run()
     {
-        moveInput = Input.GetAxis("Horizontal");
+        moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
 
